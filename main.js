8d56ce2a8590e3b6938644572c38836d31962925
eceegit <estherraphael126@gmail.com>
2023-07-07 16:30:38 +0100

    simple functionalities done

---
+++ main.js
@@ -0,0 +1,11 @@
+// // let numbers = document.querySelectorAll(".number")[0]
+// let display = document.getElementById("display")
+
+
+
+// function Display(value){
+//     display.value += value
+//     console.log(display)
+// }
+
+// Display()
